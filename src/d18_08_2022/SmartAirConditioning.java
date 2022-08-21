4ab3c11b820b0966b8b93b05cd6fa81add3d1627
Sasa Davic <sasadavic.nis@gmail.com>
2022-08-21 14:23:06 +0200

    Dopuna - zadatak za vezbu

---
+++ src/d18_08_2022/SmartAirConditioning.java
@@ -0,0 +1,55 @@
+package d18_08_2022;
+
+public class SmartAirConditioning {
+//	atribut za marku klime (npr: Samsung, Galanz, Gree, …)
+//	atribut za potrosnju elektricne energije dok hladi (npr 1kW/h)
+//	atribut za potrosnju elektricne energije dok greje (npr 2kW/h)
+//	atribut za izabranu temperaturu (minimum je 16, maksimum 35)
+//	atribut za mod (hladi/greje)
+	
+	
+		public String marka;
+		public int potrosnjaH;
+		public int potrosnjaG;
+		public int temperatura;
+		public boolean mod;
+		
+		
+//		metodu za stampu - stampa u formatu 
+//		marka - mod - termperatura
+		
+		public void stampaj() {
+			String hG = "hladi";
+			if (this.mod == true) {
+				hG = "greje";
+			}
+			System.out.println(this.marka + " - " + hG + " - " + this.temperatura);
+		}
+//		metodu koja racuna mesecnu potrosnju u kW/h, po formuli:
+////			30 * 15 * potrosnja po satu ( zavisi da li greje ili hladi)
+		
+		public int mesecnaPotrosnja(boolean mod) {
+			if (this.mod == true) {
+				return 30 * 15 * this.potrosnjaG;
+			} else {
+				return 30 * 15 * this.potrosnjaH;
+			}
+			
+		}
+		
+//		metodu koja racuna koliko klima novca potrosi za mesec dana
+//		Prvih 350 kW/h su u zelenoj zoni i 1 kW/h kosta 6din
+//		Sve ostalo se racuna kao u plavoj zoni, gde 1kW/h kosta 9 dinara
+//		Metoda vraca ukupnu cenu za taj mesec
+		
+		public int mesecnaPotrosnjaNovca(int mesecnaPotrosnjaStruje) {
+			if (mesecnaPotrosnjaStruje <= 350) {
+				int novac = mesecnaPotrosnjaStruje * 6;
+				return novac;
+			} else {
+				int novac = (mesecnaPotrosnjaStruje * 6 * 350) + (mesecnaPotrosnjaStruje - 350) * 9;
+				return novac;
+			}
+		}
+		
+}
